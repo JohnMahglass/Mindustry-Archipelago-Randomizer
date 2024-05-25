5ec4a719e24e073892d6e09b0c72032a353d3db7
JohnMahglass <johnmahglass@gmail.com>
2024-05-24 20:06:40 -0400

    merge branch

--- core/src/mindustry/randomizer/Shared.java
+++ core/src/mindustry/randomizer/Shared.java
@@ -7,5 +7,8 @@ package mindustry.randomizer;
  * @version 1.0.0 2024-05-24
  */
 public abstract class Shared {
+    /**
+     * AP Mindustry base id.
+     */
     public static final int MINDUSTRY_BASE_ID = 777000000;
 }
